67b8d0577c6bddd6cc894f7c1492d2ad465ef57b
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2022-11-16 14:21:06 +0700

    javascrip logo

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import javascript from "../public/skills/javascript.png";
 
 const Skills = () => {
   return (
@@ -12,12 +13,7 @@ const Skills = () => {
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
-                <Image
-                  src="/../public/skills/javascript.png"
-                  alt="/"
-                  width={64}
-                  height={64}
-                />
+                <Image src={javascript} alt="/" width={64} height={64} />
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>Javascript</h3>
